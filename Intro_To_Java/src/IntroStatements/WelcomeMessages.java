af9a13505a0b0689ef0b0938071ceecdc079cde1
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-10-14 09:14:52 -0400

    java code written, first statements

---
+++ Intro_To_Java/src/IntroStatements/WelcomeMessages.java
@@ -0,0 +1,24 @@
+/*
+First lines of committed Java learning. 
+
+I will be a pro, and earn tons of money. 
+
+$54.80 per hour starting April 2023. 
+
+ */
+package IntroStatements;
+
+public class WelcomeMessages {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello there world");
+        System.out.println("");
+        System.out.println("Jonathan will be making $3,100 a week");
+        System.out.println("");
+        System.out.println("Starting in April of 2023. ");
+        System.out.println("He will know Java, and MERN STACK. ");
+        
+    }
+    
+}
