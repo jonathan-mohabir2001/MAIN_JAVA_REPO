134a2e910b2986f390bfae65d20831095c7f2848
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-10-14 09:38:25 -0400

    exercise 01, from chapter 01 completed, basics of writing statments, using variables, and outputting results to console.

---
+++ Intro_To_Java/src/Chapter_01_Exercise/Exercise.java
@@ -0,0 +1,47 @@
+/*
+This Java Class will contain the exercise 
+from chapter 01. 
+
+All questions will be answered within the Main method. 
+ */
+package Chapter_01_Exercise;
+
+public class Exercise {
+
+    public static void main(String[] args) {
+
+        // 1.1 Displaying three messages. 
+        System.out.println("Welcome to Java \n "
+                + "Learning Java now \n"
+                + "Programming is gonna pay a shit ton, and its fun");
+
+        
+        
+        // 1.2 displaying I love java five times. 
+        System.out.println("I love Java ");
+        System.out.println("I love Java ");
+        System.out.println("I love Java ");     
+        System.out.println("I love Java ");
+        System.out.println("I love Java ");
+        
+        // 1.3 doing a calculation using addition. 
+       double total; 
+       total = 1+2+3+4+5+6+7+8+9+10; 
+       
+        System.out.println("Total addition is:" + total);
+        
+        // 1.4 computing expressions 
+        
+        double expression; 
+        expression = 7.5 * 6.5 - 4.5 * 3 / 47.5 - 5.5;
+        //double variable created, mathematical operation done. 
+        // statement printed on line below. 
+        System.out.println("Expression is calculated to be \n" + expression);
+        
+        
+        
+        
+        
+    }
+
+}
