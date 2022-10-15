efa8b3886b06075d80260e99d52c077bff2c6053
Jonathan Mohabir <jonamohabir416@gmail.com>
2022-10-15 10:59:49 -0400

    elementry programming directory created

---
+++ Elementry_Programming/src/CricleArea/CalculateArea.java
@@ -0,0 +1,28 @@
+/*
+This Java file will work with demonstrating the basics 
+of setting variables, reading inputs, 
+and doing calculations for finding the area of a circle. 
+ */
+package CricleArea;
+
+public class CalculateArea {
+// class name assigned necessary to the following task. 
+    
+    // all code must be written within the main argument. 
+    public static void main(String[] args) {
+// area of circle = radius * radius * pi 
+
+    double radius; 
+    double area; 
+    double pi = 3.14159; 
+    radius = 20; 
+    
+    area = radius * radius * pi; 
+    
+        System.out.println("Below is the area of a cricle...");
+        System.out.println(area);
+    
+        
+    }
+    
+}
